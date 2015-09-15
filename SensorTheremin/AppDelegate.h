a1e68b736c8fddd00eafb56c65094251b8555fd1
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-09-15 13:09:13 -0700

    Initial Commit

---
+++ SensorTheremin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SensorTheremin
+//
+//  Created by Zakk Hoyt on 9/15/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
